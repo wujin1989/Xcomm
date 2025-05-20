fa3ae2bd5db5a5ffa169d7d605a6952f92072b99
Jin.Wu <wujin.developer@gmail.com>
2025-05-20 20:34:11 +0800

    update

--- src/xcomm-base64.c
+++ src/xcomm-base64.c
@@ -1,4 +1,4 @@
-/** Copyright (c) 2025, Archermind Tech. Co., Ltd. <marketing@archermind.com>
+/** Copyright (c) 2025, Wu Jin <wujin.developer@gmail.com>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to
